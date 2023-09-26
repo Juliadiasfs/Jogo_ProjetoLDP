f123c51482f4e56a1d2261a508a6bc587449a201
Julia <117773552+Juliadiasfs@users.noreply.github.com>
2023-09-25 21:14:11 -0300

    Delete ProjetoLpd_Jogo directory

--- ProjetoLpd_Jogo/js/javascript.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("botao2").addEventListener("click", function() {
-    window.location.href = "comojogar.html";
-});
-document.getElementById("botao1").addEventListener("click", function() {
-    window.location.href = "encontreospares.html";
-});

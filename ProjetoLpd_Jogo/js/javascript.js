812a688cd77eb0ba527b50f105942d535c42750e
Julia <117773552+Juliadiasfs@users.noreply.github.com>
2023-09-25 22:10:57 -0300

    Add files via upload

---
+++ ProjetoLpd_Jogo/js/javascript.js
@@ -0,0 +1,6 @@
+document.getElementById("botao2").addEventListener("click", function() {
+    window.location.href = "comojogar.html";
+});
+document.getElementById("botao1").addEventListener("click", function() {
+    window.location.href = "encontreospares.html";
+});
